edbb10e67aba7d559ea1a576a258918df46bbf35
Imad <muhdimadduddin04@gmail.com>
2026-03-31 16:07:36 +0800

    Refactor App and ProfileCard components to implement user directory with mock data and enhance styling

--- react-day1/src/App.jsx
+++ react-day1/src/App.jsx
@@ -1,23 +1,43 @@
 import ProfileCard from "./ProfileCard";
 import Counter from './Counter';
+import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [filterRole, setFilterRole] = useState("All");
+  useEffect(() => {
+    const mockUsers = [
+    {
+      id: 1,
+      name: "Imad",
+      role: "Data Scientist",
+      skills: ["Python", "ML", "React"]
+    },
+    {
+      id: 2,
+      name: "Eiya",
+      role: "Engineer",
+      skills: ["Excel", "Powerpoint", "Communication"]
+    }
+  ];
+
+  setUsers(mockUsers);
+  }, []);
+  
   return (
     <div>
-
-      {/* Card 1 */}
-      <ProfileCard
-        name="Imad"
-        role="Data Scientist"
-        skills={["Python", "ML", "React"]}
-      />
-
-      {/* Card 2 */}
-      <ProfileCard
-        name="Eiya"
-        role="Engineer"
-        skills={["Excel", "Powerpoint", "Communication"]}
-      />
+      <h1>User Directory</h1>
+      
+      {users.map(user => (
+        <ProfileCard
+          key={user.id}
+          name={user.name}
+          role={user.role}
+          skills={user.skills}
+        />
+     ))}
 
       <Counter />
       
--- react-day1/src/ProfileCard.jsx
+++ react-day1/src/ProfileCard.jsx
@@ -3,18 +3,24 @@ import { useState } from 'react';
 function ProfileCard({ name, role, skills }) {
 
     const [isFollowing, setIsFollowing] = useState(false);
-
+    const hasSkills = skills.length > 0;
     return (
-      <div className="card">
+      <div className="profile-card">
         <h2>{name}</h2>
         <p>{role}</p>
+
+        {hasSkills ? (
         <ul>
             {skills.map((skill, index) => (
                 <li key={index}>{skill}</li>
             ))}
             </ul>
+        ) : (
+            <p>No skills listed.</p>
+        )}
+
         {/* 3. The Button with logic */}
-        <button onClick={() => setIsFollowing(!isFollowing)}>
+        <button onClick={() => setIsFollowing(prev => !prev)}>
             {isFollowing ? "Unfollow" : "Follow"}
         </button>
         </div>
